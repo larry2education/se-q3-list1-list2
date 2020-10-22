822c145c8a50bc95ab2dfdfb565a33a9fa4d83df
Larry Scott <larry2education@gmail.com>
2020-10-22 13:44:23 -0500

    Fixed Flake8 issue in list2

--- list2.py
+++ list2.py
@@ -5,7 +5,8 @@ Kenzie assignment: List2
 """
 # Your name, plus anyone who helped you with this assignment.
 # Give credit where credit is due.
-__author__ = "Larry Scott, a Smarter Way to Python, and Techie Delight-how to remove empty strings "
+__author__ = """Larry Scott, a Smarter Way to Python,
+and Techie Delight-how to remove empty strings"""
 
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
